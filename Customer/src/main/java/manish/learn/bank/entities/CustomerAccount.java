2928c4de6ffd824d39dbc0aeaf910925b7a40dbd
Manish <manishik@users.noreply.github.com>
2025-04-16 18:32:17 -0400

    Updated...

--- Customer/src/main/java/manish/learn/bank/entities/CustomerAccount.java
+++ Customer/src/main/java/manish/learn/bank/entities/CustomerAccount.java
@@ -3,13 +3,26 @@ package manish.learn.bank.entities;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Date;
+
 @Getter
 @Setter
 public class CustomerAccount {
+    // Linking Customer with Customer Id
     private Long custId;
+
+    // Account Details below
     private Long accId;
     private String accountName;
     private String accountType;
+
+    private double amount;
+
+    private Date createDate;
+
+    private Date trasanctionDate;
+
+    private Date endDate;
 
     @Override
     public String toString() {
