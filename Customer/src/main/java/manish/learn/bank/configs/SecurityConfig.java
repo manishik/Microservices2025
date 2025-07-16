954c2eb8a75127fba9681f9fdaf8f9d4d1d1bbad
Manish <manishik@users.noreply.github.com>
2025-07-16 13:50:56 -0400

    Again disable full security for making the end points work - FULL Working code

--- Customer/src/main/java/manish/learn/bank/configs/SecurityConfig.java
+++ Customer/src/main/java/manish/learn/bank/configs/SecurityConfig.java
@@ -2,7 +2,7 @@ package manish.learn.bank.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+/*import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.Customizer.withDefaults;*/
 
 
 @Configuration
