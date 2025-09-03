9ffcc0092c1961003db6cbf01615c6ba75811913
Manish <manishik@users.noreply.github.com>
2025-09-03 11:53:05 -0400

    updated

--- Customer/src/main/java/manish/learn/bank/model/Customer.java
+++ Customer/src/main/java/manish/learn/bank/model/Customer.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
--- Customer/src/main/java/manish/learn/bank/model/CustomerAddress.java
+++ Customer/src/main/java/manish/learn/bank/model/CustomerAddress.java
@@ -11,8 +11,6 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Setter
 @Getter
 @Entity
